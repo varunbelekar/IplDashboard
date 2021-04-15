8055604f3aa640b8b878fffa015f625206906938
varun <varun.belekar@somaiya.edu>
2021-04-15 17:24:07 +0530

    initial commit

---
+++ src/main/java/com/varun/ipl/IplApplication.java
@@ -0,0 +1,13 @@
+package com.varun.ipl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IplApplication.class, args);
+	}
+
+}
